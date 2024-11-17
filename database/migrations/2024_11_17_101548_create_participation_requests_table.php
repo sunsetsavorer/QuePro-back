a2a071a85d01e702fb72a3d86a0e0a73212ef343
SunsetSavorer <sunset.savorer@gmail.com>
2024-11-17 10:30:56 +0000

    create migrations

---
+++ database/migrations/2024_11_17_101548_create_participation_requests_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use App\Models\Tournament;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('participation_requests', function (Blueprint $table) {
+            $table->id();
+            $table->foreignIdFor(Tournament::class);
+            $table->string('team_name');
+            $table->string('captain_fullname');
+            $table->string('captain_phone');
+            $table->string('captain_email');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('participation_requests');
+    }
+};
